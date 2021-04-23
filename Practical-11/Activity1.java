cf6e98f228249444c02afa7a26ba641bbff7b4b4
ChakshuVerma <69800010+ChakshuVerma@users.noreply.github.com>
2021-04-23 15:18:55 +0530

    Activity 1 Java File

--- Practical-11/Activity1.java
+++ Practical-11/Activity1.java
@@ -1,28 +1,48 @@
-<?xml version="1.0" encoding="utf-8"?>
-<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:orientation="vertical"
-    android:gravity="center"
-    android:layout_height="match_parent"
-    tools:context=".MainActivity_2">
+package com.example.loginlogout;
 
-    <TextView
-        android:id="@+id/tv1"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:textStyle="bold"
-        android:textSize="40dp"
-        />
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-    <Button
-        android:id="@+id/btn_logout"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="Logout"
-        android:textColor="@android:color/white"
-        android:layout_marginTop="30dp"
-        android:background="@drawable/round_button"/>
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-</LinearLayout>
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT = "com.example.loginlogout.EXTRA_TEXT";
+
+    EditText etUsername,etPassword;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+
+        Intent intent = new Intent(this,MainActivity_2.class);
+        EditText et = findViewById(R.id.et_username);
+        String text = et.getText().toString();
+
+        intent.putExtra(EXTRA_TEXT,text);
+
+        startActivity(intent);
+    }
+}
